7bbc5da37786dd28a5ed13ae555c44e0f385332f
Tracie Masek <traciemasek@gmail.com>
2019-06-08 14:05:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,10 @@ def input_to_index(input)
   input.to_i - 1
 end
 
+def position_taken?(board, index)
+  (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
+end
+
 def move(board, index, char="X")
   board[index] = char
 end
@@ -29,9 +33,6 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index) 
 end
   
-def position_taken?(board, index)
-  (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
-end
   
 def turn(board)
   puts "Please enter 1-9:"
