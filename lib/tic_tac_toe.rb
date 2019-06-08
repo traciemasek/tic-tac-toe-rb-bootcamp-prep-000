27dc74e36d67bc9d936b5b7e1208ebce38c5705f
Tracie Masek <traciemasek@gmail.com>
2019-06-08 13:55:12 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,60 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, char)
+  board[index] = char
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index) 
+end
+  
+def position_taken?(board, index)
+  (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
+end
+  
+def turn(board)
+  puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, char)
+      display_board(board)
+    else
+      turn(board)
+    end 
+end
+
+def turn_count(board)
+    counter = 0
+    board.each do |el| 
+      if el == "X" || el == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+  
+  def current_player(board)
+    turn_count(board) % 2 == 0 ? "X" : "O"
+  end
